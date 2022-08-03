8108ae8bd0dc224d845852720ad0de3778c4a283
Sussanara <sussanaracely@gmail.com>
2022-08-03 16:52:55 +0000

    Detalles

--- src/js/helper.js
+++ src/js/helper.js
@@ -10,10 +10,10 @@ export function calculateWinner(squares) {
         [2, 4, 6]
     ];
     for (let i = 0; i < lines.length; i++) {
-        const [a, b, c] = lines[i];
-        if (squares[a] && squares[a] === squares[b] && squares[a] === squares [c]) {
+        const[a, b, c] = lines[i];
+        if(squares[a] && squares[a] === squares[b] && squares[a] === squares[c]){
             return squares[a];
-            }
+        }
     }
     return null;
 }
--- src/js/index.js
+++ src/js/index.js
@@ -3,10 +3,10 @@ import React from "react";
 import ReactDOM from "react-dom";
 
 // include your styles into the webpack bundle
-import "../styles/index.css";
+import "./index.css";
 
 //import your own components
-import Game from "./component/game";
+import Game from "./component/Game";
 
 //render your react application
-ReactDOM.render(<Game />, document.querySelector("root"));
+ReactDOM.render(<Game />, document.querySelector("#app"));
